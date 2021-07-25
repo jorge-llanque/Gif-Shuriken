757f77c1658a8653ea1893158c71bbf596ece790
jorge <georgellanque@gmail.com>
2021-07-24 21:48:28 -0400

    Add create-react-app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vamos a editar esto
         </p>
         <a
           className="App-link"
