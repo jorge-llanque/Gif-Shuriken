da33b3e0d0d2e5a3abd81dbe0e3c5283d9b31f0a
jorge <georgellanque@gmail.com>
2021-07-26 00:34:37 -0400

    Save keyword to localStorage

--- src/hooks/useGifs.js
+++ src/hooks/useGifs.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import getGifs from '../services/getGifs'
 
-export default function useGifs({keyword}) {
+export default function useGifs({keyword} = {keyword: null}) {
     const [loading, setLoading] = useState(false)
     const [gifs, setGifs] = useState([])
 
     useEffect(() => {
         setLoading(true)
-        getGifs({keyword})
+        const keywordToUse = keyword || localStorage.getItem('lastKeyword')
+
+        getGifs({keyword: keywordToUse})
             .then(gifs => {
                 setGifs(gifs)
                 setLoading(false)
+                localStorage.setItem('lastKeyword', keyword)
             })
     }, [keyword])
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     const [keyword, setKeyword] = useState('');
     const [path, pushLocation] = useLocation()
 
-    const {loading, gifs} = useGifs({keyword: 'superman'})
+    const {loading, gifs} = useGifs()
 
     const handleSubmit = evt => {
         evt.preventDefault()
@@ -25,8 +25,9 @@ export default function Home() {
             <input onChange={handleChange} type="text" value={keyword} />
             <button>Buscar</button>
         </form>
-          <h3 className="App--title" >Los gifs más populares</h3>
+        <h3>Última búsqueda</h3>
           <ListOfGifs gifs={gifs} />
+          <h3 className="App--title" >Los gifs más populares</h3>
           <ul>
               {POPULAR_GIFS.map((popularGif) => (
                   <li key={popularGif} >
