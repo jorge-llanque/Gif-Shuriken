ead776dcbd539e5f7870c2dcaa18f5613b3359db
jorge <georgellanque@gmail.com>
2021-08-16 15:45:31 -0400

    Add logo header

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React from "react";
-import './App.css';
-import { Route, Link } from "wouter";
+import { Route } from "wouter";
 import Home from "./pages/Home";
 import Detail from './pages/Detail'
 import SearchResults from "./pages/SearchResults";
 import StaticContext from "./context/StaticContext";
 import { GifsContextProvider } from "./context/GifsContext";
 import NotFound from "pages/NotFound/NotFound";
-  
+import Layout from './components/Layout';
 
 function App() {
 
   return (
-    <StaticContext.Provider value={{name: 'midudev',
-    suscribeteAlCanal: true}}>
-    <div className="App">
-      <section className="App-content">
-        <Link to="/">
-          <h1>App</h1>
-        </Link>
-
+    <StaticContext.Provider value={{name: 'midudev', suscribeteAlCanal: true}}>
         <GifsContextProvider>
-        <Route component={Home} path="/" />
-        <Route component={SearchResults} path="/search/:keyword" />
-        <Route component={Detail} path="/gif/:id" />
-        <Route component={NotFound} path="/404" />
+          <Layout>
+          <Route component={Home} path="/" />
+          <Route component={SearchResults} path="/search/:keyword" />
+          <Route component={Detail} path="/gif/:id" />
+          <Route component={NotFound} path="/404" />
+        </Layout>
         </GifsContextProvider>
-      
-      </section>
-    </div>
     </StaticContext.Provider>
   );
 }
