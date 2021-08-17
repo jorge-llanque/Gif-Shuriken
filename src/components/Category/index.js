312f0ad9318d1a230c9e2d183e4acf5026f6d9e9
jorge <georgellanque@gmail.com>
2021-08-17 04:05:06 -0400

    Add new frames

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import {Link} from 'wouter'
+import './../styles/Category.css'
 
 export default function Category({name, options}) {
 
   return (
-    <>
-       <h1>{name}</h1> 
-     <ul>
-       {
-         options.map( singleOption =>(
-           <li key={singleOption} >
-             <Link to={`/search/${singleOption}`}>
-               {singleOption}
-             </Link>
-           </li> ))
-       }
-      </ul>
-    </>
+    <div className="Category">
+       <h1 className="Category__Title">{name}</h1> 
+        <ul className="Category__Content">
+          {
+            options.map( singleOption =>(
+              <li key={singleOption} >
+                <Link to={`/search/${singleOption}`}>
+                  {singleOption}
+                </Link>
+              </li> ))
+          }
+          </ul>
+    </div>
   )
 }
