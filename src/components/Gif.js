50f37134e0c0b0e2c5cce42cd25e86bb5e3db710
jorge <georgellanque@gmail.com>
2021-08-17 03:26:37 -0400

    Change history

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -6,8 +6,10 @@ export default function Gif({title, id, url}){
     return (
         <div className="Gif" >
             <Link to={`/gif/${id}`} className="Gif--link">
-              <h4>{title}</h4>
-              <img loading='lazy' src={url} alt={title} />
+              <h4 className="Gif__Title">{title}</h4>
+              <div className="Gif__Image">
+                  <img loading='lazy' src={url} alt={title} />
+              </div>
             </Link>
         </div>
     )
