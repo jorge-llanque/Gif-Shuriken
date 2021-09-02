f0a64756f731fb250c72421cbfccdbe4e19d20a6
jorge <georgellanque@gmail.com>
2021-09-02 19:46:07 -0400

    Change out

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import { Route } from "wouter";
 import Home from "./pages/Home";
 import Detail from './pages/Detail'
 import SearchResults from "./pages/SearchResults";
-import StaticContext from "./context/StaticContext";
 import { GifsContextProvider } from "./context/GifsContext";
 import NotFound from "pages/NotFound/NotFound";
 import Layout from './components/Layout';
 
 function App() {
-
   return (
-        <GifsContextProvider>
-          <Layout>
-          <Route component={Home} path="/" />
-          <Route component={SearchResults} path="/search/:keyword" />
-          <Route component={Detail} path="/gif/:id" />
-          <Route component={NotFound} path="/404" />
-        </Layout>
-        </GifsContextProvider>
+    <GifsContextProvider>
+      <Layout>
+        <Route component={Home} path="/" />
+        <Route component={SearchResults} path="/search/:keyword" />
+        <Route component={Detail} path="/gif/:id" />
+        <Route component={NotFound} path="/404" />
+      </Layout>
+    </GifsContextProvider>
   );
 }
 
