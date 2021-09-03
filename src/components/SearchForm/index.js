1df1d7459f46242ce5a251059c06fccb28055b65
jorge <georgellanque@gmail.com>
2021-09-03 18:24:54 -0400

    Add useReducer to searchform component

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,27 +1,58 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { useLocation } from "wouter";
 import { BsSearch } from "react-icons/bs";
 
 const RATING = ["g", "pg", "pg-13", "r"];
+const ACTIONS = {
+  UPDATE_KEYWORD: 'update_keyword',
+  UPDATE_RATING: 'update_rating'
+}
+const reducer = (state, action) => {
+  switch(action.type){
+    case ACTIONS.UPDATE_KEYWORD:
+      return {
+        ...state,
+        keyword: action.payload,
+        times: state.times + 1
+      }
+    case ACTIONS.UPDATE_RATING:
+      return {
+        ...state,
+        rating: action.payload
+      }
+    default:
+      return state
+  }
+}
 
 export default function SearchForm({initialKeyword='', initialRating}) {
-  const [keyword, setKeyword] = useState(initialKeyword);
-  const [rating, setRating] = useState(decodeURIComponent(initialRating));
+  // const [keyword, setKeyword] = useState(initialKeyword);
+  // const [rating, setRating] = useState(decodeURIComponent(initialRating));
+  // const [times, setTimes] = useState(0)
+  const [state, dispatch] = useReducer(reducer, {
+    keyword: decodeURIComponent(initialKeyword),
+    rating: initialRating,
+    times:0
+  })
+  const {keyword, rating, times} = state
   const [_, pushLocation] = useLocation();
-  const [times, setTimes] = useState(0)
 
   const handleChange = (evt) => {
-    setKeyword(evt.target.value);
-    setTimes(prev => prev + 1)
+    dispatch({
+      type: ACTIONS.UPDATE_KEYWORD,
+      payload: evt.target.value
+    })
   };
   const handleSubmit = (evt) => {
     evt.preventDefault();
     pushLocation(`/search/${keyword}/${rating}`);
   };
-
   const handleChangeRating = (evt) => {
-    setRating(evt.target.value);
+    dispatch({
+      type: ACTIONS.UPDATE_RATING,
+      payload: evt.target.value
+    })
   };
 
   return (
