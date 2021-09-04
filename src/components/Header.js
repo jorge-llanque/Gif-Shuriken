dc96c52b493e4abb90d85918a247ca9698c2fa88
jorge <georgellanque@gmail.com>
2021-09-03 23:41:01 -0400

    Merge navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'wouter'
+import Navbar from './Header'
 import './styles/Header.css'
 import logo from './../assets/logo.png'
 
 export default function Header() {
   return (
     <div className="Header">
+      <Navbar/>
       <Link to="/">
         <img src={logo} className="Header__Logo" alt="shuriken logo"/>
       </Link>
