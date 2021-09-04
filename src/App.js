b52f99cfe284edc0ec18c7bdca10e9ad54717a8f
jorge <georgellanque@gmail.com>
2021-09-04 03:46:53 -0400

    Add UserContext

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import Home from "./pages/Home";
 import Detail from './pages/Detail'
 import SearchResults from "./pages/SearchResults";
 import { GifsContextProvider } from "./context/GifsContext";
+import { UserContextProvider } from 'context/UserContext'
 import NotFound from "pages/NotFound/NotFound";
 import Layout from './components/Layout';
 import Login from 'pages/Login'
 
 function App() {
   return (
-    <GifsContextProvider>
-      <Layout>
-        
-        <Route component={SearchResults} path="/search/:keyword/:rating?" />
-        <Route component={Detail} path="/gif/:id" />
-        <Route path="/login" component={Login}/>
+    <UserContextProvider>
+      <GifsContextProvider>
+        <Layout>
+          <Route component={SearchResults} path="/search/:keyword/:rating?" />
+          <Route component={Detail} path="/gif/:id" />
+          <Route path="/login" component={Login}/>
 
-        <Route component={Home} path="/" />
-        <Route component={NotFound} path="/404" />
-      </Layout>
-    </GifsContextProvider>
+          <Route component={Home} path="/" />
+          <Route component={NotFound} path="/404" />
+        </Layout>
+      </GifsContextProvider>
+    </UserContextProvider>
   );
 }
 
