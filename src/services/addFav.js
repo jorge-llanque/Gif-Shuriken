3eb670e71737d8ce78f39df062422e650eda810f
jorge <georgellanque@gmail.com>
2021-09-05 19:40:26 -0400

    Add fav services

---
+++ src/services/addFav.js
@@ -0,0 +1,33 @@
+// const ENDPOINT = 'http://localhost:8000'
+
+// export default function addFav({id, jwt}) {
+//   return fetch(`${ENDPOINT}/favs/${id}`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({jwt})
+//   }).then( res => {
+//     if(!res.ok) throw new Error('Response is NOT ok')
+//     return res.json()
+//   }).then( res => {
+//     const { favs } = res
+//     return favs
+//   })
+// }
+
+export default function addFav({ id, jwt }) {
+  const access = new Promise((res, rej) => {
+    setTimeout(() => {
+      res({
+        favs: ['1', '2'],
+      })
+    }, 1000)
+  })
+
+  return access.then(res => {
+    console.log(res)
+    const { favs } = res
+    return favs
+  })
+}
