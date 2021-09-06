16b18356087c5d83e7ee168093298db323e2e221
jorge <georgellanque@gmail.com>
2021-09-06 00:41:09 -0400

    Get favorites service

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import getFavs from 'services/getFavs'
 
 const Context = React.createContext({})
 
 export function UserContextProvider({ children }) {
   const [jwt, setJWT] = useState(() => window.sessionStorage.getItem('jwt'))
   const [favs, setFavs] = useState([])
+
+  useEffect(() => {
+    if (!jwt) return setFavs([])
+    getFavs({ jwt }).then(setFavs)
+  }, [jwt])
 
   return (
     <Context.Provider value={{ jwt, setJWT, favs, setFavs }}>
